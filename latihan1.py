b9d9477580db7503d845a841a9b2145264d50e42
Saakaaa <sakanabhan@gmail.com>
2021-10-25 20:49:25 +0700

    menambahkan file

--- latihan1.py
+++ latihan1.py
@@ -1,2 +1,10 @@
-nama = input ("siapa nama anda")
-print ("jadi nama anda adalah", nama)
+# mengambil input
+nama    =   input("Nama           :")
+namap   =   input("Nama Panggilan :")
+ttl     =   input("Tempat Lahir   :")
+umur    =   input("Umur           :")
+alamat  =   input("Alamat         :")
+telepon =   input("Nomor Telepon  :")
+
+# mengambil output
+print ("jadi nama adalah",nama,"Biasa dipanggil",namap, "Saya lahir di",ttl, "Umur saya adalah",umur, "Saya tinggal di",alamat, "Nomor Telepon saya adalah",telepon)
